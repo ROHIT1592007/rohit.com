5523376a2e4e4a25f285e4c972ffc03008a4313b
K.V.ROHIT <87985502+ROHIT1592007@users.noreply.github.com>
2021-11-29 14:12:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+  });
+
+  $(window).on('scroll load',function(){
+
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+
+    if($(window).scrollTop() > 0){
+      $('.top').show();
+    }else{
+      $('.top').hide();
+    }
+
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+
+});
+
+const menuButton=document.getElementById("menu");
+const displayPage=document.getElementById("displaypage");
+const header=document.getElementById("header");
+
+menuButton.onclick=() => {
+    displayPage.style.display="block";
+    header.style.display="none";
+}
